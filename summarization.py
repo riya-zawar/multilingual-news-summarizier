529cdc7e440d75635dda35f3e053c480a90af537
babykoala999 <114397368+babykoala999@users.noreply.github.com>
2024-04-12 21:18:38 +0530

    lsa

--- summarization.py
+++ summarization.py
@@ -5,28 +5,20 @@ nltk.download("stopwords", quiet=True)
 
 from nltk.corpus import stopwords
 
+def summarize_text(text, summary_length=3, language='english'):
+    nltk.download("punkt", quiet=True)
+    nltk.download("stopwords", quiet=True)
+
+    summarizer = LsaSummarizer()
+    stopwords_list = stopwords.words(language)
+    summarizer.stop_words = stopwords_list
+
+    summary = summarizer(text, summary_length)
+
+    return summary
+
 source_file = "original_text.txt"
-
 with open(source_file, "r", encoding='utf-8') as file:
-    text = file.readlines()
+    text = file.read()
 
-
-
-summarizer = LsaSummarizer()
-
-stopwords = stopwords.words('portuguese')
-summarizer.stop_words = stopwords
-summary =summarizer(text[0], 3)
-
-print("====== Original text =====")
-print(text)
-print("====== End of original text =====")
-
-
-
-print("\n========= Summary =========")
-
-print(" ".join(summary))
-print("========= End of summary =========")
-
-
+print(summarize_text(text,language='portuguese'))
