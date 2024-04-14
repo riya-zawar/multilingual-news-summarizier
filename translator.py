ea48cd4accc051a860bef200ffe37c58b6eea121
babykoala999 <114397368+babykoala999@users.noreply.github.com>
2024-04-14 22:43:09 +0530

    added lang detect

--- translator.py
+++ translator.py
@@ -1,6 +1,21 @@
 from deep_translator import GoogleTranslator
+import nltk
+from nltk import wordpunct_tokenize
+from nltk.corpus import stopwords
 
-def translate_text(text, source_lang='auto', target_lang='en'):
+# Download stopwords if not already downloaded
+nltk.download('stopwords')
+
+def detect_language(text):
+    words = wordpunct_tokenize(text.lower())
+    # Filter out stopwords
+    stop_words = set(stopwords.words('english'))  # Assuming English stopwords
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+    # Calculate frequency distribution of words
+    language_guess = nltk.FreqDist(filtered_words).max()
+    return language_guess
+
+def translate_text(text, target_lang='en'):
+    source_lang = detect_language(text)
     translated_text = GoogleTranslator(source=source_lang, target=target_lang).translate(text)
-    print("Translated Summary:", translated_text)  # Debugging print statement
     return translated_text
